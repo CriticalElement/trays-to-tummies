23d2631a92a8e61aba7c25d38d98037b82eac2c2
Pranav Pakanati <pranav.pakanati@gmail.com>
2024-11-25 01:36:26 -0600

    add animation to stats

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,41 @@
-<script>
+<script lang="ts">
 	import Media from '$lib/components/media.svelte';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const intersectionObserver = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					setTimeout(() => {
+						animateNumber({suffix: ' chapters', end: 36, digits: 2, element: document.querySelector('#chapters')!});
+						animateNumber({suffix: '+ items', end: 8000, digits: 4, element: document.querySelector('#items')!});
+						animateNumber({suffix: ' partnerships', end: 10, digits: 1, element: document.querySelector('#partnerships')!});
+						animateNumber({suffix: '+ impacted', end: 75000, digits: 5, element: document.querySelector('#impact')!});
+					}, 200);
+					intersectionObserver.disconnect();
+				}
+			});
+		});
+	
+		const animateNumber = ({suffix, end, digits, element} : {suffix: string, end: number, digits: number, element: HTMLElement}) => {
+			let current = 0;
+			let startTime: number | null = null;
+			const duration = 2000;
+			const step = (timestamp: number) => {
+				if (!startTime) {
+					startTime = timestamp;
+				}
+				const timeElapsed = timestamp - startTime;
+				const progress = Math.min(timeElapsed / duration, 1);
+				current = Math.floor((1 - Math.pow(1 - progress, 4)) * end);
+				element.textContent = `${current.toLocaleString(undefined, { minimumSignificantDigits: digits })}${suffix}`;
+				requestAnimationFrame(step);
+			}
+			requestAnimationFrame(step);
+		}
+	
+		intersectionObserver.observe(document.querySelector('#chapters')!);
+	});
 </script>
 
 <section id="hero">
@@ -24,22 +60,22 @@
 	<h2>benefiting our community</h2>
 	<div class="stats">
 		<div class="info">
-			<h3>36 chapters</h3>
+			<h3 id="chapters">36 chapters</h3>
 			<p>across local schools</p>
 		</div>
 		<div class="divider"></div>
 		<div class="info">
-			<h3>8,000+ items</h3>
+			<h3 id="items">8,000+ items</h3>
 			<p>donated to students</p>
 		</div>
 		<div class="divider"></div>
 		<div class="info">
-			<h3>10 partnerships</h3>
+			<h3 id="partnerships">10 partnerships</h3>
 			<p>among food banks</p>
 		</div>
 		<div class="divider"></div>
 		<div class="info">
-			<h3>75,000+ impacted</h3>
+			<h3 id="impact">75,000+ impacted</h3>
 			<p>to date</p>
 		</div>
 	</div>
@@ -54,12 +90,12 @@
 				quaerat totam molestias fugiat provident!
 			</p>
 			<Media>
-				<img src="/donation-box.jpg" alt="A donation box in a school." />
+				<img src="/food-donation.jpg" alt="A donation box in a school." />
 			</Media>
 		</div>
 		<div class="container">
 			<Media>
-				<img src="/food-donation.jpg" alt="A man carrying boxes of food to be delivered." />
+				<img src="/donation-box.jpg" alt="A man carrying boxes of food to be delivered." />
 			</Media>
 			<p>
 				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero sunt natus totam repellat
